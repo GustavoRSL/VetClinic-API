5f2bca4f3c3009f8ef47ca445ce80a2c8aa45efe
GustavoReisSouzaLima <gustavo.lima.pb@compasso.com.br>
2023-07-26 14:51:38 -0300

    feat: add route updatePet

--- src/controllers/pets.ts
+++ src/controllers/pets.ts
@@ -5,12 +5,12 @@ import Data from "../tutors.json";
 
 const createPet = async (req: Request, res: Response) => {
   try {
-    const { idTutor } = req.params;
+    const { tutorId } = req.params;
     const pet = req.body;
     const newPet: Pet = pet;
 
     const data = Data;
-    const tutor: Tutor = data.find((tutor) => tutor.id === +idTutor);
+    const tutor: Tutor = data.find((tutor) => tutor.id === +tutorId);
 
     tutor.pets.push(newPet);
 
@@ -20,4 +20,28 @@ const createPet = async (req: Request, res: Response) => {
   }
 };
 
-export { createPet };
+// Lógica para não altear ID.
+const updatePet = async (req: Request, res: Response) => {
+  try {
+    const { petId, tutorId } = req.params;
+    const pet = req.body;
+    if (!pet) return res.status(500).json({ msg: "Error!" });
+
+    const data = Data;
+    const tutor: Tutor = data.find((tutor) => tutor.id === +tutorId);
+
+    if (!tutor) return res.status(500).json({ msg: "Tutor not found!" });
+
+    let petIndex = tutor.pets.findIndex((pet: Pet) => pet.id === +petId);
+
+    if (petIndex === -1) return res.status(500).json({ msg: "Pet not found!" });
+
+    tutor.pets.splice(petIndex, 1, pet);
+
+    return res.status(200).json(pet);
+  } catch (error) {
+    return res.status(500).json({ msg: "Error!" });
+  }
+};
+
+export { createPet, updatePet };
--- src/routes/pets.ts
+++ src/routes/pets.ts
@@ -1,8 +1,9 @@
-import { createPet } from "../controllers/pets";
+import { createPet, updatePet } from "../controllers/pets";
 import { Router } from "express";
 
 const router = Router();
 
-router.post("/pet/:idTutor", createPet);
+router.post("/pet/:tutorId", createPet);
+router.put("/pet/:petId/tutor/:tutorId", updatePet);
 
 export { router };
