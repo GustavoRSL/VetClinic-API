0ce15978894106ca593bc9a1957a0e73c6175bde
GustavoReisSouzaLima <gustavo.lima.pb@compasso.com.br>
2023-07-27 09:37:28 -0300

    feat: add Schemas for tutor/pet

--- src/model/pet.ts
+++ src/model/pet.ts
@@ -1,8 +1,23 @@
-export interface Pet {
-  id: number;
+import { Document, InferSchemaType, Schema, model } from "mongoose";
+
+interface IPet extends Document {
   name: string;
   species: string;
   carry: string;
   weight: number;
-  date_of_birth: string;
+  date_of_birth: Date;
 }
+
+const petSchema = new Schema<IPet>({
+  name: { type: String, required: true },
+  species: { type: String, required: true },
+  carry: { type: String, required: true },
+  weight: { type: Number, required: true },
+  date_of_birth: { type: Date, required: true },
+});
+
+type Pet = InferSchemaType<typeof petSchema>;
+
+const pet = model<Pet>("Pet", petSchema);
+
+export { IPet, pet };
--- src/model/tutor.ts
+++ src/model/tutor.ts
@@ -1,11 +1,26 @@
-import { Pet } from "./pet";
+import { Document, InferSchemaType, Schema, model } from "mongoose";
+import { IPet, pet } from "./pet";
 
-export interface Tutor {
-  id: number;
+interface ITutor extends Document {
   name: string;
   phone: string;
   email: string;
-  date_of_birth: string;
+  date_of_birth: Date;
   zip_code: string;
-  pets: Pet[];
+  pets: IPet[];
 }
+
+const tutorSchema = new Schema<ITutor>({
+  name: { type: String, required: true },
+  phone: { type: String, required: true },
+  email: { type: String, required: true },
+  date_of_birth: { type: Date, required: true },
+  zip_code: { type: String, required: true },
+  pets: { type: [pet] },
+});
+
+type Tutor = InferSchemaType<typeof tutorSchema>;
+
+const tutor = model<Tutor>("Tutor", tutorSchema);
+
+export { ITutor, tutor };
