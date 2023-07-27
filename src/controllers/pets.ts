b245cc050bcd2e736e6d0e21c0f8e6ca1c093515
GustavoReisSouzaLima <gustavo.lima.pb@compasso.com.br>
2023-07-27 15:02:30 -0300

    fix: return object newPet

--- src/controllers/pets.ts
+++ src/controllers/pets.ts
@@ -18,35 +18,21 @@ const createPet = async (req: Request, res: Response) => {
     if (!updateTutor)
       return res.status(404).json({ error: "Tutor not found!" });
 
-    return res.status(201).json(updateTutor);
+    return res.status(201).json(newPet);
   } catch (error) {
     res.status(500).json({ error: "Error creating Pet!" });
   }
 };
 
-// // Lógica para não altear ID.
-// const updatePet = async (req: Request, res: Response) => {
-//   try {
-//     const { petId, tutorId } = req.params;
-//     const pet = req.body;
-//     if (!pet) return res.status(500).json({ msg: "Error!" });
-
-//     const data = Data;
-//     const tutor: Tutor = data.find((tutor) => tutor.id === +tutorId);
-
-//     if (!tutor) return res.status(500).json({ msg: "Tutor not found!" });
-
-//     let petIndex = tutor.pets.findIndex((pet: Pet) => pet.id === +petId);
-
-//     if (petIndex === -1) return res.status(500).json({ msg: "Pet not found!" });
-
-//     tutor.pets.splice(petIndex, 1, pet);
-
-//     return res.status(200).json(pet);
-//   } catch (error) {
-//     return res.status(500).json({ msg: "Error!" });
-//   }
-// };
+const updatePet = async (req: Request, res: Response) => {
+  try {
+    const { petId, tutorId } = req.params;
+    const updatePet: IPet = await PetModel.findByIdAndUpdate(petId, req.body);
+    return res.status(200).json(updatePet);
+  } catch (error) {
+    res.status(500).json({ error: "Error updating Pet!" });
+  }
+};
 
 // const deletePet = async (req: Request, res: Response) => {
 //   try {
@@ -65,4 +51,4 @@ const createPet = async (req: Request, res: Response) => {
 //   } catch (error) {}
 // };
 
-export { createPet };
+export { createPet, updatePet };
